75496a681ac915fa7aee2f67ad6d5629b4bbfb04
Kaviyadharshini24 <mahakavi2312>
2024-08-03 14:44:35 +0530

    changed name

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "It's me $name from paris!",
         modifier = modifier
     )
 }
